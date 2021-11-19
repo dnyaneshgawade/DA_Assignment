b40734fa8b2e8a1ae9a14e645e31cabcd2ea2b25
dnyana <dnyaneshgawade903@gmail.com>
2021-11-19 17:55:07 +0530

    [Dnyanesh] Add : performing sql programming to retrive output into csv file

--- output2.sql
+++ output2.sql
@@ -1,10 +1,6 @@
 /* retriving total usres as per total vists */
-select  distinct Total_visits As total_visits ,
-count( user_id) As customer 
-from output 
-group by Total_visits 
-order by Total_visits ;
-
+select distinct Total_visits ,
+ count( user_id), sum((select count( user_id))) over (order by Total_Visits desc) as cumulative from output group by Total_visits ;
 
 
 
@@ -12,11 +8,8 @@ order by Total_visits ;
 /* retriving data as per output format and export to csv file */
 select 'Total_visits','Customer'
 union all 
-(select  distinct Total_visits ,
- count( user_id)
- from output 
- group by Total_visits 
- order by Total_visits 
+(select distinct Total_visits ,
+ count( user_id), sum((select count( user_id))) over (order by Total_Visits desc) as cumulative from output group by Total_visits ; 
 ) 
 INTO OUTFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/output2.csv'   
 FIELDS TERMINATED BY ','  
